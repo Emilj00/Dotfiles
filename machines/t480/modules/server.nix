1264c190449e3ce657d15a1ea18dddd038656a7a
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2025-12-26 19:20:12 +0100

    Fixed server config

--- machines/t480/modules/server.nix
+++ machines/t480/modules/server.nix
@@ -1,25 +1,29 @@
-{ pkgs, lib, ... }:
-
-let
-  serversSrc = toString ../servers;
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  serversSrc = ../servers;
 
   dir = builtins.readDir serversSrc;
 
   servers = builtins.attrNames (
     lib.filterAttrs
-      (name: type:
-        type == "directory"
+    (
+      name: type:
+        type
+        == "directory"
         && builtins.pathExists (../servers + "/${name}/docker-compose.yml")
-      )
-      dir
+    )
+    dir
   );
 
   mkServer = name: {
     name = "Docker-Compose-${name}";
     value = {
       description = "Docker Server - ${name}";
-      after = [ "docker.service" ];
-      requires = [ "docker.service" ];
+      after = ["docker.service"];
+      requires = ["docker.service"];
 
       serviceConfig = {
         Type = "simple";
@@ -35,16 +39,34 @@ let
         KillMode = "control-group";
       };
 
-      wantedBy = [ "multi-user.target" ];
+      wantedBy = ["multi-user.target"];
     };
   };
 in {
   system.activationScripts.copy-servers.text = ''
-    echo "Copying servers..."
-    rm -rf /var/lib/servers
-    cp -r ${serversSrc} /var/lib/servers
+    echo "Coping servers to /var/lib/servers..."
+
+    mkdir -p /var/lib/servers
+    cp -rf ${serversSrc}/* /var/lib/servers/
+    chmod -R +w /var/lib/servers/
+  '';
+
+  system.activationScripts.restart-servers.text = ''
+    SYSTEMCTL="${pkgs.systemd}/bin/systemctl"
+    
+    echo "Restarting Docker servers..."
+    for server_dir in /var/lib/servers/*; do
+      if [ -d "$server_dir" ]; then
+        name=$(basename "$server_dir")
+        server_name="Docker-Compose-$name.service"
+      
+        if $SYSTEMCTL list-unit-files "$server_name" >/dev/null 2>&1; then
+          echo " - Restarting $server_name..."
+          $SYSTEMCTL restart "$server_name"
+        fi
+      fi
+    done
   '';
 
   systemd.services = builtins.listToAttrs (map mkServer servers);
 }
-
