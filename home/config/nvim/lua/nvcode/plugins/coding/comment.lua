b2dedd33eb64712c70c6ec6dd2966ae0ca28bed3
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2026-01-01 06:12:00 +0100

    Fixed comments

--- home/config/nvim/lua/nvcode/plugins/coding/comment.lua
+++ home/config/nvim/lua/nvcode/plugins/coding/comment.lua
@@ -1,21 +1,17 @@
 local map = vim.keymap.set
 
 local function config()
-    local comment = require("Comment.api")
+    require("nvim_comment").setup({
+        create_mappings = false,
+    })
 
-    map("n", "<C-/>", comment.toggle.linewise.current)
-    map("i", "<C-/>", comment.toggle.linewise.current)
-
-    map("v", "<C-/>", function()
-        -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "nx", false)
-        comment.toggle.linewise(vim.fn.visualmode())
-    end)
+    map({ "n", "i" }, "<C-/>", "<cmd>CommentToggle<CR>")
+    map("v", "<C-/>", ":CommentToggle<CR>")
 end
 
-local dependencies = {}
-
 return {
-    "numToStr/Comment.nvim",
+    "terrortylor/nvim-comment",
+
     config = config,
-    dependencies = dependencies
 }
+
